279fb592f35f4d2395cb826eb25e9d3c39a8c7a1
Miro Ilias <Miroslav.Ilias@umb.sk>
2020-07-26 11:15:16 +0200

    upd

---
+++ theoretical_chemistry/software_runs/ase/ase-symmetry/quartz.py
@@ -0,0 +1,12 @@
+#
+#
+#
+
+import ase.spacegroup
+quartz = ase.spacegroup.crystal(symbols=['O', 'Si'],
+                                basis=[[0.413, 0.2711, 0.2172],
+                                       [0.4673, 0, 0.3333]],
+                                spacegroup=152,
+                                cellpar=[4.9019, 4.9019, 5.3988, 90, 90, 120])
+fig, ax = plt.subplots()
+plot_atoms(quartz)

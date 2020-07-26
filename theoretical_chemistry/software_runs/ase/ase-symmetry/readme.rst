d5aa03e11dc3559b06481b89d52232614d2bbcd0
Miro Ilias <Miroslav.Ilias@umb.sk>
2020-07-26 11:11:51 +0200

    upd

--- theoretical_chemistry/software_runs/ase/ase-symmetry/readme.rst
+++ theoretical_chemistry/software_runs/ase/ase-symmetry/readme.rst
@@ -6,4 +6,9 @@ https://github.com/ajjackson/ase-tutorial-symmetry/blob/master/ase-symmetry.md
 
 milias@adf2:~/work/projects/open-collection/theoretical_chemistry/software_runs/ase/ase-symmetry/.python3 silver.py 
 Made a lattice for Ag:  <class 'ase.lattice.FCC'>
+.
+.
+.
+
+
 
--- theoretical_chemistry/software_runs/ase/ase-symmetry/silver.py
+++ theoretical_chemistry/software_runs/ase/ase-symmetry/silver.py
@@ -3,13 +3,16 @@
 #
 
 import ase.lattice
-from ase import Atoms
 
 ag_lattice = ase.lattice.FCC(a=4.09) # Face-centered cubic
 print("Made a lattice for Ag: ", type(ag_lattice))
 
 assert isinstance(ag_lattice, ase.lattice.BravaisLattice)
 
+from ase import Atoms
 ag_cell = ag_lattice.tocell()
 ag_atoms = Atoms('Ag', cell=ag_cell, pbc=True)
 print(ag_atoms.cell)
+
+from ase.visualize import view
+view(ag_atoms)

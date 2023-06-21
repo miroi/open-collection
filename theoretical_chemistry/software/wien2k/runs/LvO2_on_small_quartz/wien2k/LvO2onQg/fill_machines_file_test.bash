13bf94a8330feed6eba38294e1681e7340f28f15
Miroslav Ilias <miro.ilias@gmail.com>
2023-06-21 15:59:58 +0200

    upd

--- theoretical_chemistry/software/wien2k/runs/LvO2_on_small_quartz/wien2k/LvO2onQg/fill_machines_file_test.bash
+++ theoretical_chemistry/software/wien2k/runs/LvO2_on_small_quartz/wien2k/LvO2onQg/fill_machines_file_test.bash
@@ -2,12 +2,14 @@
 
 export SLURM_CPUS_ON_NODE=16
 
+echo "SLURM_CPUS_ON_NODE=$SLURM_CPUS_ON_NODE"
+
 echo '#' > machines
 
 counter=1
 while [ $counter -le $SLURM_CPUS_ON_NODE ] 
 do 
-  echo  "localhost" >  machines
+  echo  "localhost" >>  machines
   ((counter++))
 done
 

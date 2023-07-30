2e14da6b2f630545ef86cbae1c7c3f1516c8d430
Miroslav Ilias <miro.ilias@gmail.com>
2023-07-30 13:15:15 +0200

    upd

--- theoretical_chemistry/software/ase/runs/gpaw-ase/simple_relaxation_H2/H2_relaxation.py
+++ theoretical_chemistry/software/ase/runs/gpaw-ase/simple_relaxation_H2/H2_relaxation.py
@@ -13,6 +13,8 @@ calc = EMT()
 
 system.calc = calc
 
+# print the (decreasing) total energy for each iteration until it converges, leaving the file h2.emt.traj in the working directory. Use the command ase gui to view the trajectory file, showing each step of the optimization.
 opt = QuasiNewton(system, trajectory='h2.emt.traj')
 
+# run the optimization algorithm until all atomic forces are below 0.05 eV per Ångström.
 opt.run(fmax=0.05)

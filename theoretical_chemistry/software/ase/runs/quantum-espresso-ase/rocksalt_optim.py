f0c129e51e3e430b13cb9bf7dfa33eabb602082c
miro.ilias <miro.ilias@gmail.com>
2024-06-27 11:55:36 +0300

    u

--- theoretical_chemistry/software/ase/runs/quantum-espresso-ase/rocksalt_optim.py
+++ theoretical_chemistry/software/ase/runs/quantum-espresso-ase/rocksalt_optim.py
@@ -6,16 +6,26 @@ rocksalt = bulk('NaCl', crystalstructure='rocksalt', a=6.0)
 
 # Pseudopotentials from SSSP Efficiency v1.3.0
 #pseudopotentials = {'Na': 'na_pbe_v1.5.uspp.F.UPF', 'Cl': 'cl_pbe_v1.4.uspp.F.UPF'}
-pseudopotentials = {'Na': 'na_pbe_v1.5.uspp.F.UPF', 'Cl': 'cl_pbe_v1.4.uspp.F.UPF'}
+local_pseudopotentials = {'Na': 'Na.upf', 'Cl': 'Cl.upf'}
 
 # Optionally create profile to override paths in ASE configuration:
-profile = EspressoProfile(
+QEprofile = EspressoProfile(
     command='/usr/bin/pw.x', 
-    pseudo_dir='./'
+    pseudo_dir='./',
 )
 
+input_data = {
+    'system': {'ecutwfc': 44, 'ecutrho': 440},
+    'disk_io': 'low',  # Automatically put into the 'control' section
+}
 
-calc = Espresso(profile=profile, pseudopotentials=pseudopotentials)
+calc = Espresso(
+    profile=QEprofile,
+    pseudopotentials=local_pseudopotentials,
+    tstress=True,  # deprecated, put in input_data
+    tprnfor=True,  # deprecated, put in input_data
+    input_data=input_data,
+    )
 
 rocksalt.calc = calc
 
@@ -29,4 +39,5 @@ opt = LBFGS(rocksalt)
 opt.run(fmax=0.005)  # This will run a geometry optimization using ASE's LBFGS algorithm
 
 # Print lattice constant...
+print("Obtained optimized lattice constant, a=")
 print((8 * rocksalt.get_volume() / len(rocksalt)) ** (1.0 / 3.0))

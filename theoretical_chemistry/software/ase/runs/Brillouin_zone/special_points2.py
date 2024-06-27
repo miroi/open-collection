0c82c579d8979a2341f726216a7add784d444897
miro.ilias <miro.ilias@gmail.com>
2024-06-27 09:57:41 +0300

    u

---
+++ theoretical_chemistry/software/ase/runs/Brillouin_zone/special_points2.py
@@ -0,0 +1,10 @@
+from ase.build import bulk
+#from ase.build import cell
+si = bulk('Si', 'diamond', a=5.459)
+si_lat = si.cell.get_bravais_lattice()
+print(list(si_lat.get_special_points()))
+path = si.cell.bandpath('GXW', npoints=100)
+print(path.kpts.shape)
+
+
+

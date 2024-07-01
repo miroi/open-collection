ec626d4360a673613a64c16d68d92b8b90db4f6e
Miro Ilias <miro.ilias@gmail.com>
2024-07-01 15:34:11 +0300

    u

--- theoretical_chemistry/software/ase/runs/adsorption_study/bulk.py
+++ theoretical_chemistry/software/ase/runs/adsorption_study/bulk.py
@@ -6,11 +6,15 @@ from ase.db import connect
 db = connect('bulk.db')
 #for symb in ['Al', 'Ni', 'Cu', 'Pd', 'Ag', 'Pt', 'Au', 'Tl']:
 for symb in ['Al', 'Ni', 'Cu', 'Pd', 'Ag', 'Pt', 'Au']:
+#  https://en.wikipedia.org/wiki/Cubic_crystal_system
     atoms = bulk(symb, 'fcc')
 #    atoms = bulk(symb, 'bcc')
     atoms.calc = EMT()
+
     eos = calculate_eos(atoms)
     v, e, B = eos.fit()  # find minimum
+
+
     # Do one more calculation at the minimu and write to database:
     atoms.cell *= (v / atoms.get_volume())**(1 / 3)
     atoms.get_potential_energy()

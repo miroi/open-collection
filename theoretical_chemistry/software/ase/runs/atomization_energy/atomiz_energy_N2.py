a2509869dce64f1980f00810a823bd075139c4d5
miro.ilias <miro.ilias@gmail.com>
2024-07-07 19:56:27 +0300

    u

--- theoretical_chemistry/software/ase/runs/atomization_energy/atomiz_energy_N2.py
+++ theoretical_chemistry/software/ase/runs/atomization_energy/atomiz_energy_N2.py
@@ -5,8 +5,10 @@ atom = Atoms('N', calculator=EMT())
 e_atom = atom.get_potential_energy()
 
 d = 1.1
-molecule = Atoms('2N', [(0., 0., 0.), (0., 0., d)])
-molecule.set_calculator(EMT())
+molecule = Atoms('2N', [(0., 0., 0.), (0., 0., d)], calculator=EMT() )
+#molecule.set_calculator(EMT())
+
+
 e_molecule = molecule.get_potential_energy()
 
 e_atomization = e_molecule - 2 * e_atom

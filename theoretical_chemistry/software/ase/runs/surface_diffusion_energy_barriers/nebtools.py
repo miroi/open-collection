e9a0a8e4e68f02ee52f893bdfb1a070da5b9d701
miro.ilias <miro.ilias@gmail.com>
2024-07-17 21:55:23 +0300

    u

---
+++ theoretical_chemistry/software/ase/runs/surface_diffusion_energy_barriers/nebtools.py
@@ -0,0 +1,31 @@
+#
+#  For more customizable analysis of the output of many NEB jobs, you can use the ase.mep.NEBTools class
+#
+import matplotlib.pyplot as plt
+
+from ase.io import read
+from ase.mep import NEBTools
+
+images = read('neb.traj@-5:')
+
+nebtools = NEBTools(images)
+
+# Get the calculated barrier and the energy change of the reaction.
+Ef, dE = nebtools.get_barrier()
+
+# Get the barrier without any interpolation between highest images.
+Ef, dE = nebtools.get_barrier(fit=False)
+
+# Get the actual maximum force at this point in the simulation.
+max_force = nebtools.get_fmax()
+
+# Create a figure like that coming from ASE-GUI.
+fig = nebtools.plot_band()
+fig.savefig('diffusion-barrier.png')
+
+# Create a figure with custom parameters.
+fig = plt.figure(figsize=(5.5, 4.0))
+ax = fig.add_axes((0.15, 0.15, 0.8, 0.75))
+nebtools.plot_band(ax)
+fig.savefig('diffusion-barrier.png')
+

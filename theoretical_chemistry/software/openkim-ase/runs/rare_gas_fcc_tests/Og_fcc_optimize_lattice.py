d1b3822dbb620ec0a9555a007507b322309c0af6
miro.ilias <miro.ilias@gmail.com>
2024-07-23 23:01:02 +0300

    u

--- theoretical_chemistry/software/openkim-ase/runs/rare_gas_fcc_tests/Og_fcc_optimize_lattice.py
+++ theoretical_chemistry/software/openkim-ase/runs/rare_gas_fcc_tests/Og_fcc_optimize_lattice.py
@@ -1,12 +1,25 @@
 #
 #
 
-from ase.lattice.cubic import FaceCenteredCubic
+from ase.constraints import ExpCellFilter, StrainFilter
 from ase.calculators.kim.kim import KIM
+from ase.build import bulk
+from ase.optimize import LBFGS, BFGS, FIRE
 
-atoms = FaceCenteredCubic(symbol='Og', latticeconstant=5.25, size=(1,1,1))
+Og_fcc = bulk(name='Og',crystalstructure='fcc', a=5.25)
 calc = KIM("LJ_ElliottAkerson_2015_Universal__MO_959249795837_003")
-atoms.calc = calc
+Og_fcc.calc = calc
 
-energy = atoms.get_potential_energy()
-print("3D Og fcc potential energy: {} eV".format(energy))
+print ("Og fcc cell befor optim:",Og_fcc.cell)
+energy = Og_fcc.get_potential_energy()
+print("Og fcc potential energy before opt: {} eV".format(energy))
+
+Og_fcc.calc = calc
+
+Og_fcc = StrainFilter(Og_fcc)
+opt = LBFGS(Og_hcp, trajectory="lbfgs_Og_fcc.traj")
+opt.run()
+
+print ("Og fcc cell AFTER optim:",Og_fcc.atoms.cell)
+energy = Og_fcc.get_potential_energy()
+print("Og fcc potential energy AFTER opt: {} eV".format(energy))

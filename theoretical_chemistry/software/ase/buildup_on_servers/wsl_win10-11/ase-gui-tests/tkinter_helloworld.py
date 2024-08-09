caf8413a49e4f4290819b943604a854cc0daca60
Miro Ilias <miro.ilias@gmail.com>
2024-08-09 15:38:46 +0300

    u

---
+++ theoretical_chemistry/software/ase/buildup_on_servers/wsl_win10-11/ase-gui-tests/tkinter_helloworld.py
@@ -0,0 +1,12 @@
+#
+# https://docs.python.org/3/library/tkinter.html#a-hello-world-program
+#
+
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()

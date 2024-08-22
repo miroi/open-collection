45d20bc7e75de13de4831f7191840805efaca0b9
Miro Ilias <miro.ilias@gmail.com>
2024-08-22 19:30:21 +0300

    upd

--- theoretical_chemistry/software/ase/runs/create_slabs/Al111_with_Hadsorbed/Al111_with_H.py
+++ theoretical_chemistry/software/ase/runs/create_slabs/Al111_with_Hadsorbed/Al111_with_H.py
@@ -6,7 +6,7 @@ from ase.build import fcc111, add_adsorbate
 from ase.visualize import view
 slab = fcc111('Al', size=(2,2,3))
 add_adsorbate(slab, 'H', 1.5, 'ontop')
-slab.center(vacuum=10.0, axis=2)
+slab.center(vacuum=15.0, axis=2)
 
 slab.write("Al111_H.in")
 

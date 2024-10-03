86f561f02d6551338755f6c1415bb7fec282092e
Miro Ilias <milias@DESKTOP-7OTLCGO>
2024-10-03 19:14:24 +0300

    upd

--- theoretical_chemistry/software/ase/runs/simple_relaxation_H2/H2_relaxation_02.py
+++ theoretical_chemistry/software/ase/runs/simple_relaxation_H2/H2_relaxation_02.py
@@ -5,6 +5,7 @@
 from ase import Atoms
 from ase.calculators.emt import EMT
 from ase.optimize import QuasiNewton
+from ase.geometry import get_distances
 import numpy as np
 
 #d = starting geometry distance of H2 molecule
@@ -13,5 +14,7 @@ for d in np.arange(1.6, 2.8, 0.1):
    system = Atoms('H2',positions=[[0.0, 0.0, 0.0],[0.0, 0.0, d]])
    calc = EMT()
    system.calc = calc
-   opt = QuasiNewton(system)
+   opt = QuasiNewton(system,logfile='opt.logfile')
    opt.run(fmax=0.02)
+   print ("number of geometry steps ",opt.get_number_of_steps()+1)
+   print("optimized distance d(H-H)=",system.get_distance(0,1))

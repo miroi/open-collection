3296da788449a78d1978f310c1952e186961d8f2
Miro Ilias <miro.ilias@gmail.com>
2024-10-09 01:05:15 +0300

    u

--- theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/ase-rocksalt.py
+++ theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/ase-rocksalt.py
@@ -5,15 +5,23 @@ from ase.optimize import LBFGS
 rocksalt = bulk('NaCl', crystalstructure='rocksalt', a=6.0)
 
 # Pseudopotentials from SSSP Efficiency v1.3.0
-pseudopotentials = {'Na': 'na_pbe_v1.5.uspp.F.UPF', 'Cl': 'cl_pbe_v1.4.uspp.F.UPF'}
+pseudopotentials = {'Na':'na_pbe_v1.5.uspp.F.UPF', 'Cl':'cl_pbe_v1.4.uspp.F.UPF'}
 
 # Optionally create profile to override paths in ASE configuration:
 #profile = EspressoProfile(
 #    command='/path/to/pw.x', pseudo_dir='/path/to/pseudopotentials'
 #)
+#profile = EspressoProfile( pseudo_dir='./')
 
-#calc = Espresso(profile=profile, pseudopotentials=pseudopotentials)
-calc = Espresso(pseudopotentials=pseudopotentials)
+input_data = { 
+    'calculation': 'scf', 'tprnfor': True, 'tstress': True,
+    'system': {'ecutwfc': 40, 'ecutrho': 320},
+    'disk_io': 'low',  # Automatically put into the 'control' section
+}
+
+#calc = Espresso(profile=profile, pseudopotentials=pseudo_dir)
+##calc = Espresso(pseudopotentials=pseudopotentials)
+calc = Espresso(pseudopotentials=pseudopotentials, input_data=input_data,)
 
 rocksalt.calc = calc
 
@@ -21,7 +29,8 @@ rocksalt.get_potential_energy()  # This will run a single point calculation
 
 opt = LBFGS(rocksalt)
 
-opt.run(fmax=0.005)  # This will run a geometry optimization using ASE's LBFGS algorithm
+#opt.run(fmax=0.005)  # This will run a geometry optimization using ASE's LBFGS algorithm
+opt.run(fmax=0.02)  # This will run a geometry optimization using ASE's LBFGS algorithm
 
 # Print lattice constant...
-print((8 * rocksalt.get_volume() / len(rocksalt)) ** (1.0 / 3.0))
+print("latt.constant a=",(8 * rocksalt.get_volume() / len(rocksalt)) ** (1.0 / 3.0))
--- theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/readme.rst
+++ theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/readme.rst
@@ -5,5 +5,15 @@ https://wiki.fysik.dtu.dk/ase/ase/calculators/espresso.html
 
 get PP: https://www.quantum-espresso.org/pseudopotentials/
 
+place them into commpon PP dir:
 
+sudo cp cl_pbe_v1.4.uspp.F.UPF na_pbe_v1.5.uspp.F.UPF  /usr/share/espresso/pseudo/.
 
+run
+~~~~
+python3 ase-rocksalt.py
+       Step     Time          Energy          fmax
+LBFGS:    0 00:55:18    -1750.250480        0.000000
+6.0
+
+...forces not calculated, to be fixed ! 

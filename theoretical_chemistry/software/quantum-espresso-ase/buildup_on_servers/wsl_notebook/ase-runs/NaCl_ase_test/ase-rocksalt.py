fdb468d2aa32b1fa98c8228b96860374912abc26
Miro Ilias <milias@DESKTOP-7OTLCGO>
2024-10-10 15:47:36 +0300

    upd

--- theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/ase-rocksalt.py
+++ theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/ase-rocksalt.py
@@ -1,11 +1,14 @@
 from ase.build import bulk
 from ase.calculators.espresso import Espresso, EspressoProfile
 from ase.optimize import LBFGS
+from ase.filters import FrechetCellFilter
 
 rocksalt = bulk('NaCl', crystalstructure='rocksalt', a=6.0)
 
+print("unptimized rocksalt lattice constant a=",(8 * rocksalt.get_volume() / len(rocksalt)) ** (1.0 / 3.0))
+
 # Pseudopotentials from SSSP Efficiency v1.3.0
-pseudopotentials = {'Na':'na_pbe_v1.5.uspp.F.UPF', 'Cl':'cl_pbe_v1.4.uspp.F.UPF'}
+local_pseudopotentials = {'Na':'na_pbe_v1.5.uspp.F.UPF', 'Cl':'cl_pbe_v1.4.uspp.F.UPF'}
 
 # Optionally create profile to override paths in ASE configuration:
 #profile = EspressoProfile(
@@ -13,24 +16,29 @@ pseudopotentials = {'Na':'na_pbe_v1.5.uspp.F.UPF', 'Cl':'cl_pbe_v1.4.uspp.F.UPF'
 #)
 #profile = EspressoProfile( pseudo_dir='./')
 
-input_data = { 
+NaCl_input_data = { 
     'calculation': 'scf', 'tprnfor': True, 'tstress': True,
     'system': {'ecutwfc': 40, 'ecutrho': 320},
     'disk_io': 'low',  # Automatically put into the 'control' section
 }
 
+ecf = FrechetCellFilter(rocksalt)
+
 #calc = Espresso(profile=profile, pseudopotentials=pseudo_dir)
 ##calc = Espresso(pseudopotentials=pseudopotentials)
-calc = Espresso(pseudopotentials=pseudopotentials, input_data=input_data,)
+calc = Espresso(pseudopotentials=local_pseudopotentials, input_data=NaCl_input_data,)
 
 rocksalt.calc = calc
+#ecf.calc = calc
 
-rocksalt.get_potential_energy()  # This will run a single point calculation
+#rocksalt.get_potential_energy()  # This will run a single point calculation
+ecf.get_potential_energy()  # This will run a single point calculation
 
-opt = LBFGS(rocksalt)
+#opt = LBFGS(rocksalt)
+opt = LBFGS(ecf)
 
 #opt.run(fmax=0.005)  # This will run a geometry optimization using ASE's LBFGS algorithm
 opt.run(fmax=0.02)  # This will run a geometry optimization using ASE's LBFGS algorithm
 
 # Print lattice constant...
-print("latt.constant a=",(8 * rocksalt.get_volume() / len(rocksalt)) ** (1.0 / 3.0))
+print("QE optimized rocksalt lattice constant a=",(8 * rocksalt.get_volume() / len(rocksalt)) ** (1.0 / 3.0))
--- theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/readme.rst
+++ theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/readme.rst
@@ -1,19 +1,28 @@
 QE ase test
 ===========
 
+configure
+~~~~~~~~~
+https://wiki.fysik.dtu.dk/ase/ase/calculators/calculators.html#calculator-configuration
 https://wiki.fysik.dtu.dk/ase/ase/calculators/espresso.html
 
 get PP: https://www.quantum-espresso.org/pseudopotentials/
 
 place them into commpon PP dir:
-
-sudo cp cl_pbe_v1.4.uspp.F.UPF na_pbe_v1.5.uspp.F.UPF  /usr/share/espresso/pseudo/.
+  sudo cp cl_pbe_v1.4.uspp.F.UPF na_pbe_v1.5.uspp.F.UPF  /usr/share/espresso/pseudo/.
 
 run
-~~~~
-python3 ase-rocksalt.py
-       Step     Time          Energy          fmax
-LBFGS:    0 00:55:18    -1750.250480        0.000000
-6.0
+~~~
+for hint see https://gitlab.com/ase/ase/-/issues/1546#note_2152201647
 
-...forces not calculated, to be fixed ! 
+milias@DESKTOP-7OTLCGO:~/work/git-projects/open-collection/theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/wsl_notebook/ase-runs/NaCl_ase_test/.python3 ase-rocksalt.py
+unptimized rocksalt lattice constant a= 6.0
+       Step     Time          Energy          fmax
+LBFGS:    0 15:44:09    -1750.250480        2.274927
+LBFGS:    1 15:44:10    -1750.459055        2.010169
+LBFGS:    2 15:44:11    -1751.262469        0.766752
+LBFGS:    3 15:44:12    -1751.454381        0.275210
+LBFGS:    4 15:44:13    -1751.491306        0.084493
+LBFGS:    5 15:44:14    -1751.494509        0.006087
+QE optimized rocksalt lattice constant a= 7.534520921896282
+

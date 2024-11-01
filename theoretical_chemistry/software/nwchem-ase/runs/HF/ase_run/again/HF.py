511405c2a3f08e2e87d88b369fd0a072c268dfd1
Miro Ilias <miro.ilias@gmail.com>
2024-11-01 13:54:23 +0300

    u

---
+++ theoretical_chemistry/software/nwchem-ase/runs/HF/ase_run/again/HF.py
@@ -0,0 +1,18 @@
+from ase import Atoms
+from ase.calculators.nwchem import NWChem
+from ase.io import write
+ 
+d=0.91
+hf = Atoms('HF',positions=[[0, 0, 0],[0, 0, d]])
+
+#hf.calc = NWChem(xc='PBE',basis='6-311G', property=dict('mulliken'))
+hf.calc = NWChem(xc='PBE',basis='6-311G', property='mulliken')
+#hf.calc = NWChem(xc='PBE',basis='6-311G')
+
+en = hf.get_potential_energy() 
+
+print('HF PBE run, d(HF)=',d,'Ang; Energy=',en,' eV')
+#print(hf.calc.get_charges())
+
+
+

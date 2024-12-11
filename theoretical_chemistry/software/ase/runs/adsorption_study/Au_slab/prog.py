f67909617aeefee0df7480df424236df708774ff
Miro Ilias <miro.ilias@gmail.com>
2024-12-11 16:58:56 +0300

    u

--- theoretical_chemistry/software/ase/runs/adsorption_study/Au_slab/prog.py
+++ theoretical_chemistry/software/ase/runs/adsorption_study/Au_slab/prog.py
@@ -1,6 +1,9 @@
 #
-# try to calculate Hg on Au slab
+# try to calculate adsorption energy of Pt on Au slab
 #
+# see https://www.sciencedirect.com/science/article/pii/S0009261407006513
+#
+from ase import Atoms
 from ase.build import bulk
 from ase.calculators.emt import EMT
 from ase.eos import calculate_eos
@@ -12,10 +15,8 @@ from ase.optimize import BFGS
 #for symb in ['Al', 'Ni', 'Cu', 'Pd', 'Ag', 'Pt', 'Au']:
 symb='Au'
 #  https://en.wikipedia.org/wiki/Cubic_crystal_system
-atoms = bulk(symb, 'fcc')
-#    atoms = bulk(symb, 'bcc')
+atoms = bulk(symb,'fcc')
 atoms.calc = EMT()
-
 eos = calculate_eos(atoms)
 v, e, B = eos.fit()  # find minimum
 
@@ -23,21 +24,36 @@ v, e, B = eos.fit()  # find minimum
 atoms.cell *= (v / atoms.get_volume())**(1 / 3)
 a = atoms.cell[0, 1] * 2
 atoms.get_potential_energy()
-print(symb, 'fcc crystal energy:',atoms.get_potential_energy())
+print(symb, 'fcc crystal energy:',atoms.get_potential_energy(), ' a=',a)
 
-n = 3
-atoms = fcc111('Au', (1, 1, n), a=a)
-add_adsorbate(atoms, 'Pt', height=1.0, position='fcc')
-#   print(symb," fcc111 lattice constant,  a=",a)
+n = 3;
+atoms = fcc111(symb, (1, 1, n), a=a)
+atoms.calc=EMT()
+atoms.get_forces()
+en_slab = atoms.get_potential_energy()
+print('energy of the Au slab =',en_slab)
 
-    # Constrain all atoms except the adsorbate:
+
+add_atom='Pt'
+#add_atom='C' # this is veryfied with program (-3.547)
+a1=Atoms(add_atom)
+a1.calc = EMT()
+en_atom=a1.get_potential_energy()
+print(a1.symbols,'single atom pot.energy =',en_atom)
+
+add_adsorbate(atoms, add_atom, height=1.0, position='fcc')
+# Constrain all atoms except the adsorbate:
 fixed = list(range(len(atoms) - 1))
 atoms.constraints = [FixAtoms(indices=fixed)]
 
 atoms.calc = EMT()
 opt = BFGS(atoms, logfile=None)
 opt.run(fmax=0.01)
+en_atom_on_surface=atoms.get_potential_energy()
 
-print('done...')
+#print('atoms ', atoms)
+print('energy of atom plus surface system :',en_atom_on_surface)
 
+en_ads = en_atom_on_surface - en_atom - en_slab
+print('adsorption energy of Pt on Au fcc111 =', en_ads)
 
---
+++ theoretical_chemistry/software/ase/runs/adsorption_study/Au_slab/readme.rst
@@ -0,0 +1,13 @@
+Au slab with Pt addatom
+=======================
+
+exp.data :  https://www.sciencedirect.com/science/article/pii/S0009261407006513
+
+
+python3 prog.py
+Au fcc crystal energy: -0.00013514121662172585  a= 4.056168091621226
+energy of the Au slab = 0.47677295552592724
+Pt single atom pot.energy = 5.85
+energy of atom plus surface system : 0.6714933807144492
+adsorption energy of Pt on Au fcc111 = -5.655279574811478
+

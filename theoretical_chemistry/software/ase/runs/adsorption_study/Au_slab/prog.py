a82b8dbaa028a48ef152c8872c65d4300c9a4584
Miro Ilias <miro.ilias@gmail.com>
2024-12-11 16:29:08 +0300

    u

---
+++ theoretical_chemistry/software/ase/runs/adsorption_study/Au_slab/prog.py
@@ -0,0 +1,43 @@
+#
+# try to calculate Hg on Au slab
+#
+from ase.build import bulk
+from ase.calculators.emt import EMT
+from ase.eos import calculate_eos
+from ase.db import connect
+from ase.build import fcc111, add_adsorbate
+from ase.constraints import FixAtoms
+from ase.optimize import BFGS
+
+#for symb in ['Al', 'Ni', 'Cu', 'Pd', 'Ag', 'Pt', 'Au']:
+symb='Au'
+#  https://en.wikipedia.org/wiki/Cubic_crystal_system
+atoms = bulk(symb, 'fcc')
+#    atoms = bulk(symb, 'bcc')
+atoms.calc = EMT()
+
+eos = calculate_eos(atoms)
+v, e, B = eos.fit()  # find minimum
+
+# Do one more calculation at the minimum 
+atoms.cell *= (v / atoms.get_volume())**(1 / 3)
+a = atoms.cell[0, 1] * 2
+atoms.get_potential_energy()
+print(symb, 'fcc crystal energy:',atoms.get_potential_energy())
+
+n = 3
+atoms = fcc111('Au', (1, 1, n), a=a)
+add_adsorbate(atoms, 'Pt', height=1.0, position='fcc')
+#   print(symb," fcc111 lattice constant,  a=",a)
+
+    # Constrain all atoms except the adsorbate:
+fixed = list(range(len(atoms) - 1))
+atoms.constraints = [FixAtoms(indices=fixed)]
+
+atoms.calc = EMT()
+opt = BFGS(atoms, logfile=None)
+opt.run(fmax=0.01)
+
+print('done...')
+
+

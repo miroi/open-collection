e7159630ffa67d72ee09bacce54a97b7704f0926
Miroslav Ilias <M.Ilias@umb.sk>
2025-01-05 01:05:58 +0300

    up

--- theoretical_chemistry/software/nwchem-ase/runs/ase_nwchem_h2_run/Cl2_run/ase_nwchem_Cl2.py
+++ theoretical_chemistry/software/nwchem-ase/runs/ase_nwchem_h2_run/Cl2_run/ase_nwchem_Cl2.py
@@ -6,5 +6,5 @@ from ase.io import write
 d=2.0 # close to exp.distance 228 pm
 cl2 = Atoms('Cl2',positions=[[0, 0, 0],[0, 0, d]])
 cl2.calc = NWChem(dft=dict(maxiter=500,xc='B3LYP'),basis='6-31++g')
-opt = BFGS(br2, trajectory='cl2.traj')
+opt = BFGS(cl2, trajectory='cl2.traj')
 opt.run(fmax=0.01)

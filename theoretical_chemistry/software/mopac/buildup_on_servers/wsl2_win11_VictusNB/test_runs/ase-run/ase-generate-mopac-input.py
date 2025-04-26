694e85fd2f3357bb9762a61f8a8bc628aec55230
Miro Ilias <miro.ilias@gmail.com>
2025-04-27 00:22:33 +0300

    up

--- theoretical_chemistry/software/mopac/buildup_on_servers/wsl2_win11_VictusNB/test_runs/ase-run/ase-generate-mopac-input.py
+++ theoretical_chemistry/software/mopac/buildup_on_servers/wsl2_win11_VictusNB/test_runs/ase-run/ase-generate-mopac-input.py
@@ -8,7 +8,7 @@ from ase.calculators.mopac import MOPAC
 atoms = molecule('O2')
 atoms.calc = MOPAC(label='O2')
 
-atoms.get_potential_energy() 
+#atoms.get_potential_energy() 
 
 #eigs = atoms.calc.get_eigenvalues()
 #somos = atoms.calc.get_somo_levels()

8eaedcfa64c5f70ddf62e6c1ef84ee839ec4d7ba
Miro Ilias <miro.ilias@gmail.com>
2025-06-06 16:39:55 +0300

    u

---
+++ theoretical_chemistry/software/dft-dispersion-corr/dft-d4/runs/test_fortran_api/handling-of-geometries-and-structure/example.f90
@@ -0,0 +1,27 @@
+subroutine example
+   use mctc_env, only : wp
+   use mctc_io, only : structure_type, new
+   implicit none
+   type(structure_type) :: mol
+   real(wp), allocatable :: xyz(:, :)
+   integer, allocatable :: num(:)
+
+   num = [6, 1, 1, 1, 1]
+   xyz = reshape([ &
+     &  0.00000000000000_wp, -0.00000000000000_wp,  0.00000000000000_wp, &
+     & -1.19220800552211_wp,  1.19220800552211_wp,  1.19220800552211_wp, &
+     &  1.19220800552211_wp, -1.19220800552211_wp,  1.19220800552211_wp, &
+     & -1.19220800552211_wp, -1.19220800552211_wp, -1.19220800552211_wp, &
+     &  1.19220800552211_wp,  1.19220800552211_wp, -1.19220800552211_wp],&
+     & [3, size(num)])
+
+   call new(mol, num, xyz, charge=0.0_wp, uhf=0)
+
+   ! ...
+end subroutine example
+
+program test_example
+
+call example
+
+end program

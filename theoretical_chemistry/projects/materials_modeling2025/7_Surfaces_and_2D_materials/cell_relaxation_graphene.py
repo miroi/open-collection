04e5a063f592fecb701bf33d941f93fae3d8f9c8
Miro Ilias <miro.ilias@gmail.com>
2025-08-29 11:50:20 +0300

    u

--- theoretical_chemistry/projects/materials_modeling2025/7_Surfaces_and_2D_materials/cell_relaxation_graphene.py
+++ theoretical_chemistry/projects/materials_modeling2025/7_Surfaces_and_2D_materials/cell_relaxation_graphene.py
@@ -63,11 +63,11 @@ atoms = Atoms(
 # ==============================================
 # Set QE bin directory 
 #qe_bin = "/home/dsen/work/bin/qe-7.4.1_serial"
-qe_bin = "/home/dsen/work/bin/qe-7.4.1"
+qe_bin = "/usr/bin"
 
 # Job commands
 #pw_command = f'{qe_bin}/bin/pw.x'
-pw_command = f'mpirun -np 4 {qe_bin}/bin/pw.x'
+pw_command = f'mpirun -np 8 {qe_bin}/pw.x'
 
 pw_profile = EspressoProfile(
     command=pw_command,

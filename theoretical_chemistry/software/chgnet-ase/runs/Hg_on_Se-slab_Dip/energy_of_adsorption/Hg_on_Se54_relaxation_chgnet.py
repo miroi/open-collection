077d44c1499847f58e54b4b92191d524b5076e51
Miro Ilias <miro.ilias@gmail.com>
2025-11-23 21:14:20 +0300

    u

---
+++ theoretical_chemistry/software/chgnet-ase/runs/Hg_on_Se-slab_Dip/energy_of_adsorption/Hg_on_Se54_relaxation_chgnet.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from ase import Atoms
+from ase.io import write, read
+from ase.io.trajectory import Trajectory
+
+#
+# https://github.com/CederGroupHub/chgnet?tab=readme-ov-file#direct-inference-static-calculation
+#
+from chgnet.model.model import CHGNet
+from pymatgen.core import Structure
+from chgnet.model import StructOptimizer
+from chgnet.model.dynamics import TrajectoryObserver
+
+
+import numpy as np
+import sys
+from ase.filters import UnitCellFilter 
+
+sys.stdout.flush()
+
+chgnet = CHGNet.load()
+
+# ==============================================
+# 2. Atomic Structure 
+# ==============================================
+print("\n  Read in QE preoptimized structure HgSe54_QEoptimized.vasp ")
+structure = Structure.from_file('HgSe54_QEoptimized.vasp')
+#
+# set up calculator
+# https://chgnet.lbl.gov/api#method-relax
+
+relaxer = StructOptimizer()
+print("\nStarting cell relaxation by ASE", flush=True)
+
+#traj=TrajectoryObserver(structure)
+result = relaxer.relax(structure,save_path='relaxation_path.traj')
+#result = relaxer.relax(structure)
+
+#traj.save('path.trj')
+#print('traj.compute_energy :',traj.compute_energy() )
+
+print("CHGNet relaxed structure", result["final_structure"])
+print("relaxed total energy in eV:", result['trajectory'].energies[-1])
+
+# Save final structure
+write('final_relaxed_structure.vasp', structure, format='vasp', direct=False)
+print("\nFinal relaxed structure saved to: final_relaxed_structure.vasp", flush=True)
+
+# ==============================================
+traj = Trajectory('relaxation.traj', 'w', structure)

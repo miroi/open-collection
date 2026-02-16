8e2fccae00e94bdd7ec12042b7582c729d813c02
Miro Ilias <miro.ilias@gmail.com>
2026-02-16 13:17:08 +0300

    u

--- theoretical_chemistry/software/openbabel-ase/buildups/ase-openbabel/ase_pybel_energy_test.py
+++ theoretical_chemistry/software/openbabel-ase/buildups/ase-openbabel/ase_pybel_energy_test.py
@@ -5,10 +5,11 @@
 from ase.io import read
 #from ase.calculators.openbabel import OpenBabel
 #from openbabel import OpenBabel
-import openbabel 
+from openbabel import pybel, openbabel
 
 # Read a molecule using OpenBabel's SMILES parser
-atoms = read('C1=CC=CC=C1', format='smi')
+#atoms = read('C1=CC=CC=C1', format='smi')
+atoms =  pybel.readstring("smi",'C1=CC=CC=C1')
 
 # Attach the OpenBabel UFF calculator
 #atoms.calc = OpenBabel(forcefield='uff')

f8b1c2d8dd9a0f8fbfb9e19439e0fb4a61c5cee4
Miro Ilias <miro.ilias@gmail.com>
2026-03-11 21:14:27 +0300

    u

--- theoretical_chemistry/projects/adsorptions_with_ase/PbO_on_gold/abdelrham/PbO_gold111_V2.py
+++ theoretical_chemistry/projects/adsorptions_with_ase/PbO_on_gold/abdelrham/PbO_gold111_V2.py
@@ -24,8 +24,7 @@ def get_relaxed_energy(atoms, label):
 
 # Load Model
 model = CHGNet.load()
-calc = CHGNetCalculator(model=model, use_device="gpu")
-#calc = CHGNetCalculator(model=model, use_device="cpu")
+calc = CHGNetCalculator(model=model)
 
 atoms = bulk('Au', 'fcc', a=4.0, cubic=True)
 atoms.calc = calc
@@ -114,44 +113,50 @@ e_pbo_free = get_relaxed_energy(pbo_mol, "isolated_pbo")
 
 print('The Energy of PbO (Isolated) = ',e_pbo_free)
 
+surface_atoms = slab[slab.positions[:, 2] > (z_surf - 0.5)]
+ref_atom = surface_atoms[0]
+ref_x = ref_atom.position[0]
+ref_y = ref_atom.position[1]
 # d is the distance between nearest neighbor Au atoms on the (111) surface
 d = a_opt / np.sqrt(2)
 
 manual_sites = {
-    'ontop':  [0.0, 0.0],
-    'bridge': [d / 2.0, 0.0],
-    'fcc':    [d / 2.0, d / (2.0 * np.sqrt(3))],
-    'hcp':    [d / 2.0, -d / (2.0 * np.sqrt(3))]
+    'ontop':  [ref_x, ref_y],
+    'bridge': [ref_x + d / 2.0, ref_y],
+    'fcc':    [ref_x + d / 2.0, ref_y + d / (2.0 * np.sqrt(3))],
+    'hcp':    [ref_x + d / 2.0, ref_y - d / (2.0 * np.sqrt(3))]
 }
-
-print(f"\n{'Site':<10} | {'Orientation':<12} | {'E_total (eV)':<10} | {'E_ads (eV)':<10}")
+angles = np.arange(0, 360, 45)
+print(f"\n{'Site':<10} | {'Angle':<12} | {'E_total (eV)':<10} | {'E_ads (eV)':<10}")
 print("-" * 50)
 
 results = []
 initial_height = 2.3
 
 for site_name, (sx, sy) in manual_sites.items():
-    system = slab.copy()
+    for angle in angles:
+        system = slab.copy()
+    
+        # Place Pb
+        #pb_pos = np.array([sx, sy, z_surf + initial_height])
+        #system.append(Atom('Pb', position=pb_pos))
+    
+        mol = Atoms('PbO', positions=[[0, 0, 0], [bond_length, 0, 0]])
+        mol.rotate(angle, 'z', center='COM') 
+        mol.positions += [sx, sy, z_surf + initial_height]
+        system.extend(mol)
 
-    # Place Pb
-    pb_pos = np.array([sx, sy, z_surf + initial_height])
-    system.append(Atom('Pb', position=pb_pos))
-
-    # Place O 
-    o_pos = pb_pos + np.array([bond_length, 0.0, 0.0])
-    system.append(Atom('O', position=o_pos))
-
-    system.set_pbc(True)
-    system.wrap()
-    label = f"flat_{site_name}"
-
-    try:
-        e_total = get_relaxed_energy(system, label)
-        e_ads = e_total - (e_slab + e_pbo_free)
-        print(f"{site_name:<10} | {'Flat':<12} | {e_total:>10.4f} | {e_ads:>10.4f}")
-        results.append((e_ads, site_name))
-    except Exception as e:
-        print(f"{site_name:<10} | {'Flat':<12} | FAILED: {e}")
+        system.set_pbc(True)
+        system.wrap()
+        label = f"flat_{site_name}_angle_{angle}"
+    
+        try:
+            e_total = get_relaxed_energy(system, label)
+            e_ads = e_total - (e_slab + e_pbo_free)
+            print(f"{site_name:<10} | {f"{angle}":<12} | {e_total:>10.4f} | {e_ads:>10.4f}")
+            results.append((e_ads, site_name))
+        except Exception as e:
+            print(f"{site_name:<10} | {'Flat':<12} | FAILED: {e}")
 
 if results:
     best_e, best_site = min(results)

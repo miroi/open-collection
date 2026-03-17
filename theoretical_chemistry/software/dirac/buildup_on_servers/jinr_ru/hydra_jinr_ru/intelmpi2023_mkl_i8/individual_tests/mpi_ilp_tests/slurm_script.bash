0a8bc99f3cd2e038b1a49f5dc44a76c0e037168d
Miroslav Ilias <milias@jinr.ru>
2026-03-17 16:09:40 +0300

    u

--- theoretical_chemistry/software/dirac/buildup_on_servers/jinr_ru/hydra_jinr_ru/intelmpi2023_mkl_i8/individual_tests/mpi_ilp_tests/slurm_script.bash
+++ theoretical_chemistry/software/dirac/buildup_on_servers/jinr_ru/hydra_jinr_ru/intelmpi2023_mkl_i8/individual_tests/mpi_ilp_tests/slurm_script.bash
@@ -5,6 +5,8 @@
 #SBATCH --constraint=cascade
 #SBATCH --qos=short
 #SBATCH --time=00:01:00
+#SBATCH -N 1 -n 2
+#SBATCH -o log_slurm_job.%j.%N.std_out_err
 
 date
 hostname
@@ -12,14 +14,17 @@ hostname
 module add  intel/v2023.1.0
 module list
 
+which mpirun; mpirun --version
+
 which mpiicc; mpiicc --version
 mpiicc -ilp64  mpi_test.c -o mpi_test.x
-./mpi_text.x
+mpirun -np  $SLURM_NTASKS ./mpi_test.x
 
 which mpiifort; mpiifort --version
 mpiifort -i8 check_ilp64.f90 -o check_ilp64.x
-./check_ilp64.x
+mpirun -np  $SLURM_NTASKS ./check_ilp64.x
 
-
+mpiifort  check_ilp64.f90 -o check_ilp64_i4.x
+mpirun -np  $SLURM_NTASKS ./check_ilp64_i4.x
 
 

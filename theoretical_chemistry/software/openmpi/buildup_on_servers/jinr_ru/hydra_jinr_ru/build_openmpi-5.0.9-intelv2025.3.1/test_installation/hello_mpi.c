5b22874c9f175965c4b4f654c5ca3041aeb22961
Miroslav Ilias <milias@jinr.ru>
2026-03-31 15:56:58 +0300

    upd

---
+++ theoretical_chemistry/software/openmpi/buildup_on_servers/jinr_ru/hydra_jinr_ru/build_openmpi-5.0.9-intelv2025.3.1/test_installation/hello_mpi.c
@@ -0,0 +1,13 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    printf("Hello world from rank %d out of %d processors\n", world_rank, world_size);
+    MPI_Finalize();
+    return 0;
+}
+

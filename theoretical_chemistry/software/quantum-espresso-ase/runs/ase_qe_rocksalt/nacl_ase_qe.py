c8cff6a1e7b928daf00bc9193475d7fa36a14ca8
Miroslav Ilias <milias@jinr.ru>
2026-04-08 15:11:38 +0300

    u

--- theoretical_chemistry/software/quantum-espresso-ase/runs/ase_qe_rocksalt/nacl_ase_qe.py
+++ theoretical_chemistry/software/quantum-espresso-ase/runs/ase_qe_rocksalt/nacl_ase_qe.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from ase import Atoms
 from ase.calculators.espresso import Espresso, EspressoProfile
-from dftd4.ase import DFTD4
+##from dftd4.ase import DFTD4
 from ase.io import read, write
 from ase.optimize import BFGS
 from ase.io.trajectory import Trajectory
 #from ase.constraints import FixAtoms
 #from ase.filters import UnitCellFilter 
 from ase.calculators.mixing import SumCalculator
-from dftd4.interface import DampingParam
-from dftd4.parameters import get_damping_param
+##from dftd4.interface import DampingParam
+##from dftd4.parameters import get_damping_param
 import os
 import numpy as np
 import sys
@@ -25,7 +25,7 @@ pseudopotentials = {
 
 input_data = {
     'control': {
-        'prefix': 'NaCl_D4',
+        'prefix': 'NaCl',
         'outdir': './tmp',
         'verbosity': 'low',
         'tstress': True,
@@ -88,7 +88,7 @@ except Exception as e:
 qe_bin = os.environ["qe_bin"]
 
 # Main QE calculation with full parallelization
-pw_command = f'mpirun -v -np {os.environ["SLURM_NTASKS"]} -machinefile nodes.{os.environ["SLURM_JOB_PARTITION"]}.{os.environ["SLURM_JOB_ID"]} --bind-to core:overload-allowed {qe_bin}/bin/pw.x -npool {os.environ["SLURM_JOB_NUM_NODES"]}'
+pw_command = f'/lustre/projects/m/milias/work/software/openmpi/openmpi-5.0.9-intelv2025.3.1/install_openmpi-5.0.9-intelv2025.3.1/bin/mpirun -v -np {os.environ["SLURM_NTASKS"]} -machinefile nodes.{os.environ["SLURM_JOB_PARTITION"]}.{os.environ["SLURM_JOB_ID"]} --bind-to core:overload-allowed {qe_bin}/bin/pw.x -npool {os.environ["SLURM_JOB_NUM_NODES"]}'
 #pw_command = f'mpirun -np {os.environ["SLURM_NTASKS"]} -machinefile nodes.{os.environ["SLURM_JOB_PARTITION"]}.{os.environ["SLURM_JOB_ID"]} {qe_bin}/bin/pw.x -npool {os.environ["SLURM_JOB_NUM_NODES"]}'
 
 profile = EspressoProfile(
@@ -104,27 +104,14 @@ qe_calc = Espresso(
 #    koffset=(1, 1, 0) 
 )
 
-custom_params = {
-    's6': 1.0, # Two-body dispersion scaling
-    's9': 1.0, # Higher-order dispersion scaling 
-    'alp': 16.0, # Damping attenuation steepness   
-    's8': 0.95948085, # Three-body dispersion scaling
-    'a1': 0.38574991, # Damping function parameter 1
-    'a2': 4.80688534, # Damping function parameter 2
-}
-dftd4_calc = DFTD4(verbose=True, params_tweaks=custom_params)
 
-combined_calc = SumCalculator([qe_calc, dftd4_calc])
-atoms.calc = combined_calc
+atoms.calc = qe_calc 
 
 # ==============================================
 # 5. Run Cell Relaxation (Slab, Cell Angles Fixed)
 # ==============================================
 
-print("Starting atom relaxation with QE+D4 forces via ASE...")
-
-print("PBE default DFT-D4 parameters :", get_damping_param("pbe"), flush=True)
-print("Custom DFT-D4 parameters      :", custom_params, flush=True)
+print("Starting atom relaxation with QE forces via ASE...")
 
 # Create trajectory file
 traj = Trajectory('relaxation.traj', 'w', atoms)
@@ -141,22 +128,12 @@ try:
     
     # Calculate final energies
     qe_energy = qe_calc.get_potential_energy(relaxed_atoms)
-    d4_energy = dftd4_calc.get_potential_energy(relaxed_atoms)
-    total_energy = combined_calc.get_potential_energy(relaxed_atoms)
+    total_energy = qe_energy
     
     # Print final energies
     print("\n=== Final Relaxation Results ===")
     print(f"\nEnergy Components:")
-    print(f"  QE Electronic Energy:    {qe_energy:>12.6f} eV", flush=True)
-    print(f"  DFT-D4 Dispersion:       {d4_energy:>12.6f} eV", flush=True)
-    print(f"  Total Energy (QE + D4):  {total_energy:>12.6f} eV", flush=True)
-    
-    # Energy consistency check
-    energy_diff = abs(total_energy - (qe_energy + d4_energy))
-    if energy_diff <= 0.001:
-        print(f"\nEnergy check: consistent", flush=True)
-    else:
-        print(f"\nEnergy check: inconsistent (Δ={energy_diff:.6f} eV)", flush=True)
+    print(f"  Total Energy (QE ):  {total_energy:>12.6f} eV", flush=True)
     
     # Get final forces and stress
     forces = opt.atoms.get_forces() # From ASE relaxation steps

0179a2afed75cd43e21093d4b58332ed174aefff
Miro Ilias <miro.ilias@gmail.com>
2026-04-24 23:35:51 +0300

    u

---
+++ theoretical_chemistry/software/quantum-espresso-ase/buildup_on_servers/jinr_ru/hydra_jinr_ru-Govorun/intelmpi2025.3.1_libxc_mkl_scalapack/test_Fl-on-Se54/ase-driven-qe-dftd4/convert_qe_inp_to_vasp/convert_qe2vasp.py
@@ -0,0 +1,6 @@
+from ase.io import read, write
+# Read QE input
+structure = read('Se54_r_Fl_2steprelax.in', format='espresso-in')
+# Write VASP POSCAR
+write('FlSe54.vasp', structure, format='vasp', vasp5=True, direct=True)
+

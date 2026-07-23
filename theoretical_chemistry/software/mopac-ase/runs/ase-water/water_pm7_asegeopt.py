68d0758b8ae8f9e15a61666cb48431ca121aeafe
Miro Ilias <miro.ilias@gmail.com>
2026-07-23 06:51:07 +0300

    u

--- theoretical_chemistry/software/mopac-ase/runs/ase-water/water_pm7_asegeopt.py
+++ theoretical_chemistry/software/mopac-ase/runs/ase-water/water_pm7_asegeopt.py
@@ -8,23 +8,27 @@ from ase.optimize import BFGS
 #os.environ['ASE_MOPAC_COMMAND'] = 'path/to/mopac'
 #os.environ['ASE_MOPAC_COMMAND'] = '/home/milias/work/software/mopac/mopac-23.2.3-linux/bin/mopac'
 #os.environ['ASE_MOPAC_COMMAND'] = '/usr/bin/mopac'
-os.environ['ASE_MOPAC_COMMAND'] = '/home/miroi/work/software/mopac/mopac-23.1.2-linux/bin/mopac'
+#os.environ['ASE_MOPAC_COMMAND'] = '/home/miroi/work/software/mopac/mopac-23.1.2-linux/bin/mopac'
+os.environ['ASE_MOPAC_COMMAND'] = '/home/miroi/work/software/mopac/mopac-23.1.2-linux/bin/mopac PREFIX.mop 2> /dev/null'
 
 # 2. Generate the initial water molecule
 system = molecule('H2O')
+r_oh1 = system.get_distance(0, 1)
+r_oh2 = system.get_distance(0, 2)
+angle_hoh = system.get_angle(1, 0, 2)
+print(f"\n--- ASE prepared H2O molecule geometry  ---")
+print(f"O-H1 Bond Length : {r_oh1:.3f} Å")
+print(f"O-H2 Bond Length : {r_oh2:.3f} Å")
+print(f"H-O-H Bond Angle : {angle_hoh:.1f}°")
 
 # 3. Suppress MOPAC's stdout by redirecting it to devnull during calculation
 # Note: MOPAC prints directly to stdout bypasses ASE's logfile controls
-#sys.stdout = open(os.devnull, 'w')
 system.calc = MOPAC(method='PM7', task='1SCF GRADIENTS')
 
 # 4. Run optimization with a clean logfile and restore standard output
 # Setting logfile='optimization.log' redirects the BFGS step information
 opt = BFGS(system, trajectory='opt.traj', logfile='optimization.log')
-opt.run(fmax=0.05)
-
-# Restore standard output to print the final results to the terminal
-sys.stdout = sys.__stdout__
+opt.run(fmax=0.01)
 
 # 5. Extract finalized structural parameters
 r_oh1 = system.get_distance(0, 1)
